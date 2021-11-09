e1fdca8b15007d3d306c880c66f3e0ae0f1c0b15
ihsanbara94 <ihsanbatubara94@gmail.com>
2021-11-09 11:26:34 +0700

    add navigation n button component

---
+++ components/atoms/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <button :class="`bg-`">
+          {{content}}
+      </button>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        content : {
+            type : String,
+            default : "Daftar sekarang"
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
