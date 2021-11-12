caa9f6550b8c837ce01df06d7022b77c906adac2
Ibnu Hazar <ibnuhazar101@gmail.com>
2021-11-12 14:17:28 +0700

    set custom css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+// prettier-ignore
 module.exports = {
   mode: "jit",
   purge: [
@@ -10,8 +11,22 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        'primary': '#0a7dfa'
+      },
       backgroundImage: {
         mentorBg: "url('~/static/bg-mentor.jpg')",
+      },
+      fontSize: {
+        '14': '14px',
+        '16': '16px',
+        '18': '18px',
+        '20': '20px',
+        '24': '24px',
+        '26': '26px',
+        '32': '32px',
+        '40': '40px',
+        '52': '52px',
       },
     },
   },
