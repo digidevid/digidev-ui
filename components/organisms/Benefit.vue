6e516aa80cbb5820373d04c7fc4a796bdf3f8b71
fshlarf <faishal.arif218@gmail.com>
2021-11-14 12:35:21 +0700

    change style benefit section

--- components/organisms/Benefit.vue
+++ components/organisms/Benefit.vue
@@ -4,16 +4,27 @@
       <img class="w-full" src="/images/vector/Vector123.png" alt="" />
     </div>
     <h2 class="text-4xl tracking-tight text-center">
-      <span class="font-bold">Keunggulan</span> DigiDev
+      <span class="font-bold">Keunggulan</span> Digidev
     </h2>
 
-    <div class="relative px-[163px] pt-16 flex lg:flex-row space-x-4 z-50">
+    <div
+      class="
+        relative
+        max-w-[1120px]
+        pt-[72px]
+        m-auto
+        flex
+        lg:flex-row
+        space-x-4
+        z-50
+      "
+    >
       <div class="relative w-2/5 px-[30px] py-10 rounded-xl shadow-xl bg-white">
         <div class="absolute w-14 h-14 bg-primary rounded-full top-[-25px]">
           <img class="vector" src="/images/vector/rating.png" alt="rating" />
         </div>
-        <h4 class="text-2xl font-bold text-start">Berpengalaman</h4>
-        <p class="text-start pt-2">
+        <h4 class="text-[22px] mt-5 font-bold text-start">Berpengalaman</h4>
+        <p class="text-start pt-2 text-[18px]">
           Mentor dengan segudang <span class="font-bold">pengalaman</span>
           yang berkarir di startup ternama.
         </p>
@@ -27,8 +38,10 @@
             alt="exclusive"
           />
         </div>
-        <h4 class="text-2xl font-bold text-start">Privat dan Ekslusif</h4>
-        <p class="text-start pt-2">
+        <h4 class="text-[22px] mt-5 font-bold text-start">
+          Privat dan Ekslusif
+        </h4>
+        <p class="text-start pt-2 text-[18px]">
           Kelas dilaksanakan secara <span class="font-bold">offline</span>,
           privat dan eksklusif dengan mengikuti prokes.
         </p>
@@ -42,10 +55,10 @@
             alt="curriculum"
           />
         </div>
-        <h4 class="text-2xl font-bold text-start">
+        <h4 class="text-[22px] mt-5 font-bold text-start">
           Kurikulum Sesuai Dunia Kerja
         </h4>
-        <p class="text-start pt-2">
+        <p class="text-start pt-2 text-[18px]">
           Kurikulum yang dipelajari disesuaikan dengan
           <span class="font-bold">kebutuhan dunia kerja</span> saat ini.
         </p>
@@ -54,8 +67,10 @@
         <div class="absolute w-14 h-14 bg-primary rounded-full top-[-25px]">
           <img class="vector" src="/images/vector/money.png" alt="money" />
         </div>
-        <h4 class="text-2xl font-bold text-start">Relatif Terjangkau</h4>
-        <p class="text-start pt-2">
+        <h4 class="text-[22px] mt-5 font-bold text-start">
+          Relatif Terjangkau
+        </h4>
+        <p class="text-start pt-2 text-[18px]">
           Dengan asas saling tolong menolong membuat kelas
           <span class="font-bold">lebih terjangkau</span>
         </p>
