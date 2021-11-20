7b7b0ec622b0b277b6cd236b3222abc9cf2a0881
Ibnu Hazar <ibnuhazar101@gmail.com>
2021-11-20 09:41:43 +0700

    fix head title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "digidev-ui",
+    title: "digidev",
     htmlAttrs: {
       lang: "en",
     },
