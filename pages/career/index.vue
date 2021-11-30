84285d45c3fa85c94c6a49aafdc47a5a637545d6
ihsanbara94 <92623664+ihsanbara94@users.noreply.github.com>
2021-11-30 10:55:39 +0700

    Ihsanbara (#25)
    * responsive for PortofolioAlumni
    
    * thank-you component added
    
    * delete container ini portofolioAlumni
    
    * comp payment-methods using responsive
    
    * delete hamburger menu from register,payment page,thank-you
    
    * restyle register,payment-page,thank-you
    
    * re-style payment on clicked bank
    
    * fix some problem in register index
    
    * payment page nb- minus params class and price
    
    * add Pricing and class route in Payment
    
    * add bank default object in payment
    
    * add bank default
    
    * fix default chosedBank data in payment page
    
    * delete some useless code
    
    Co-authored-by: Ibnu Hazar <ibnuhazar101@gmail.com>

---
+++ pages/career/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <Navbar class="-mb-32 md:-mb-20" />
+    <div
+      class="grid lg:mb-8 mb-6 justify-center"
+      v-for="(career, id) in dataCareer"
+      :key="id"
+    >
+      <FormCard :career="career" />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "~/components/organisms/Footer";
+import Navbar from "../career/views/Navbar.vue";
+import FormCard from "./views/FormCard.vue";
+export default {
+  components: { Footer, Navbar, FormCard },
+  data() {
+    return {
+      dataCareer: [
+        {
+          className: " Mentor Frontend Developer",
+          salary: "Rp2.000.000 - Rp5.000.000",
+          requirement: [
+            "Degree in computer science or related field.",
+            "Understanding of key design principles.",
+            "Proficiency in HTML, CSS, JavaScript, and jQuery.",
+            "Understanding of server-side CSS.",
+            "Experience with graphic design applications such as Adobe Illustrator.",
+            "Experience with responsive and adaptive design.",
+          ],
+        },
+        {
+          className: " Mentor Backend Developer",
+          salary: "Rp2.000.000 - Rp5.000.000",
+          requirement: [
+            "Degree in computer science or related field.",
+            "Understanding of key design principles.",
+            "Proficiency in HTML, CSS, JavaScript, and jQuery.",
+            "Understanding of server-side CSS.",
+            "Experience with graphic design applications such as Adobe Illustrator.",
+            "Experience with responsive and adaptive design.",
+          ],
+        },
+        {
+          className: " Mentor Mobile Developer",
+          salary: "Rp2.000.000 - Rp5.000.000",
+          requirement: [
+            "Degree in computer science or related field.",
+            "Understanding of key design principles.",
+            "Proficiency in HTML, CSS, JavaScript, and jQuery.",
+            "Understanding of server-side CSS.",
+            "Experience with graphic design applications such as Adobe Illustrator.",
+            "Expdesign.",
+          ],
+        },
+        {
+          className: " Mentor Full Stack Developer",
+          salary: "Rp2.000.000 - Rp5.000.000",
+          requirement: [
+            "Degree in computer science or related field.",
+            "Understanding of key design principles.",
+            "Proficiency in HTML, CSS, JavaScript, and jQuery.",
+            "Understanding of server-side CSS.",
+            "Experience with graphic design applications such as Adobe Illustrator.",
+            "Expdesign.",
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ pages/career/views/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div :class="`navigation ${setHeight}`">
+    <div
+      class="
+        flex
+        w-full
+        justify-start
+        items-center
+        px-[24px]
+        sm:px-[24px]
+        md:px-[160px]
+        :px-[160px]
+        pt-9
+      "
+    >
+      <div>
+        <nuxt-link to="/"
+          ><img class="max-w-[56px]" src="~/static/digidev-bw.png" alt=""
+        /></nuxt-link>
+      </div>
+    </div>
+    <div
+      :class="`lg:px-[160px] md:px-[160px] sm:px-[24px] px-[24px] space-y-5 ${setPaddingTop}`"
+    >
+      <p
+        class="
+          text-white text-26
+          sm:text-26
+          md:text-52
+          lg:text-52
+          leading-relaxed
+          max-w-[1116px]
+        "
+      >
+        DigiDev Career <br />
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    setHeight() {
+      return this.typeClass ? "" : "h-[350px]";
+    },
+    setPaddingTop() {
+      return this.typeClass ? "pt-[40px] pb-[800px]" : "my-[40px]";
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.navigation {
+  background-image: url("~/static/images/hero/hero6.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+}
+</style>
