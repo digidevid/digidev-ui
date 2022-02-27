f93312fe2295fa19617f5da410b336f74b1e71dd
fshlarf <faishal.arif218@gmail.com>
2022-02-27 18:53:08 +0700

    add page webinar

---
+++ pages/webinar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Redirect to webinar form registration</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    window.location = "https://forms.gle/jkcwRe3ipapgeZEV7";
+  },
+};
+</script>
+
+<style></style>
