07310f07b3629f41f7628452a27ee9ace1c472c8
Ibnu Hazar <ibnuhazar101@gmail.com>
2022-03-30 20:26:17 +0700

    add page redirect to digicafe

---
+++ pages/cafe.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-screen h-screen relative">
+    <div class="flex justify-center items-center w-full h-full">
+      <div>
+        <p>Redirecting to Digi-Cafe...</p>
+        <div
+          class="w-full h-3 bg-gray-300 animate-pulse rounded-full mt-2"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    window.location = "https://digidevid.github.io/Digi-Cafe/";
+  },
+};
+</script>
+
+<style></style>
